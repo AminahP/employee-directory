cc840e72015aa4ad6711bd86c8ae5a1dfb2aa1fa
Aminahp <aypitt7@gmail.com>
2022-10-05 18:04:20 -0400

    styling

---
+++ src/components/EmployeeListItem.js
@@ -0,0 +1,12 @@
+export default function EmployeeListItem({ img, name, role, division}) {
+    return (
+      <div className="list-item">
+        {/* <EmployeeImg img={img} alt={name} /> */}
+        <div className="content">
+          <h4>{name}</h4>
+          <p>{role}</p>
+          <p>{division}</p>
+        </div>
+      </div>
+    );
+  }
